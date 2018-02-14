2fb5009d9b7a8f51ca738e5174cfb12306b89948
yuvann <yuvan2244@gmail.com>
2018-02-14 19:51:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname));	
 app.set('port', (process.env.PORT || 5000))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Welcome Guys')
 })
